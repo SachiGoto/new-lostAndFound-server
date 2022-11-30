bfa4a27a52f7bfc2513351983fb244e5cfc4e206
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-11-29 16:47:34 -0800

    made some changes

--- server.js
+++ server.js
@@ -20,21 +20,28 @@ app.set("view engine", "ejs");
 app.use(express.static("uploads"));
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Methods",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
+
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "Origin, X-Requested, Content-Type, Accept Authorization"
+//   )
+//   if (req.method === "OPTIONS") {
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "POST, PUT, PATCH, GET, DELETE"
+//     )
+//     return res.status(200).json({})
+//   }
+//   next()
+// })
 
 app.use(cors());
 
