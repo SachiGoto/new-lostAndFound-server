9631ed6da29ddf0395200c76399da2fa91a9cfe3
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-11-29 17:58:39 -0800

    test 2

--- server.js
+++ server.js
@@ -220,10 +220,10 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
   }
 );
 
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server running on port ${process.env.PORT}`);
-// });
-
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+
+// app.listen(3000, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
